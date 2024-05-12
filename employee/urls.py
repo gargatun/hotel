4fca827582c795d35c54763eee87361b75b46fa6
gargatun <matvey.zamula@mail.ru>
2024-05-12 16:22:56 +0300

    добавлены номера, небольшие изменения

--- employee/urls.py
+++ employee/urls.py
@@ -1,5 +1,6 @@
 from django.urls import path
 from employee import views
+from django.contrib.auth.views import LogoutView
 
 
 app_name = 'employee'
@@ -7,5 +8,6 @@ app_name = 'employee'
 urlpatterns = [
     path('login/', views.LoginUser.as_view(), name='login'),
     path('', views.IndexView.as_view(), name='index'),  # Make sure this corresponds to `success_url`
+    path('logout/', LogoutView.as_view(next_page='employee:index'), name='logout'),
 
 ]
